a2815fc76747c236cc5e3ba49e476bf4be6e0d9e
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2023-02-26 02:24:11 -0600

    Adding post, and delete request

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -22,5 +22,54 @@ namespace APIBackend.Controllers
         {
             return await _context.Patient.ToListAsync();
         }
+
+        [HttpGet]
+        [Route("id")]
+        public async Task<ActionResult<Patient>> get_patient(int id)
+        {
+            var patient = await _context.Patient.FindAsync(id);
+
+            return (patient == null) ? NotFound() : patient;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Patient>> post_patient(Patient patient)
+        {
+            if (patient == null)
+            {
+                return BadRequest();
+            } else
+            {
+                _context.Patient.Add(patient);
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch(DbUpdateException)
+                {
+                    // catch some exception
+                    throw;
+                }
+
+                return CreatedAtAction("Patient created", patient);
+            }
+            
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> delete_patient(int id)
+        {
+            var patient = await _context.Patient.FindAsync(id);
+
+            if(patient == null)
+            {
+                return NotFound();
+            } else
+            {
+                _context.Patient.Remove(patient);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+        }
     }
 }
