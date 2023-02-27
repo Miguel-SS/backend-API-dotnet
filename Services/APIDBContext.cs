0536a0a06551edb81ca1eee2862d18f0f33cb898
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2023-02-27 00:28:54 -0600

    db context updated with doctor, hospital and appointment models

--- Services/APIDBContext.cs
+++ Services/APIDBContext.cs
@@ -9,5 +9,11 @@ namespace APIBackend.Services
         public APIDBContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Patient>? Patient { get; set; }
+
+        public DbSet<Doctor>? Doctor { get; set; }
+
+        public DbSet<Hospital>? Hospital { get; set; }
+
+        public DbSet<Appointment>? Appointment { get; set; }
     }
 }
