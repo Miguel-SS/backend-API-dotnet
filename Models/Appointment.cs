66d9b3f95dbdbc7b0a4c785319afda9d8def1e07
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2023-02-27 00:23:25 -0600

    Hospital and appointment class model added

---
+++ Models/Appointment.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIBackend.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int? fk_id_patient { get; set; }
+        public int? fk_id_doctor { get; set; }
+        public int? fk_id_hospital { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Hour { get; set; }
+        public string? Name { get; set; }
+        public double? TotalCost { get; set; }
+        public bool? Active { get; set; }
+
+        //public Appointment()
+        //{
+        //    this.Patient = new Patient();
+        //    this.Doctor = new Doctor();
+        //    this.Hospital = new Hospital();
+        //}
+
+        public Patient? Patient { get; set; }
+        public Doctor? Doctor { get; set; }
+        public Hospital? Hospital { get; set; }
+    }
+}
---
+++ Models/Hospital.cs
@@ -0,0 +1,10 @@
+﻿namespace APIBackend.Models
+{
+    public class Hospital
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? TimeAttention { get; set; }
+    }
+}
