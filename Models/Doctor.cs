99e2570b4c928c69264682ddc9ed175e3c0f22a7
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2023-02-26 23:40:32 -0600

    Doctor class model added

---
+++ Models/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace APIBackend.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Premium { get; set; }
+    }
+}
